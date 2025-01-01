3629689f4dfc546d24910c0c682662b940f344d6
dawidplatek <dawidplatek.eu.pl@gmail.com>
2025-01-01 21:41:53 +0100

    Week 7 lesson 12 - business logic (part I)

--- Diary/ViewModels/MainViewModel.cs
+++ Diary/ViewModels/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Diary.Commands;
-using System.Windows;
+using Diary.Models;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
 
 namespace Diary.ViewModels
@@ -9,27 +10,87 @@ namespace Diary.ViewModels
         public MainViewModel()
         {
             RefreshStudentsCommand = new RelayCommand(RefreshStudents);
+
+            Students = new ObservableCollection<Student>
+            {
+                new Student { FirstName = "Tomasz", LastName = "Kowalski", Group = new Group {Id = 1 } },
+                new Student { FirstName = "Marek", LastName = "Nowak", Group = new Group {Id = 2 } },
+                new Student { FirstName = "Stanisław", LastName = "Nowakowski", Group = new Group {Id = 1 } }
+            };
+
+            InitGroups();
         }
 
         public ICommand RefreshStudentsCommand { get; set; }
 
-        private string _test = "XXX";
 
-        public string Test
+
+        private Student _selectedStudent;
+
+        public Student SelectedStudent
         {
-            get { return _test; }
+            get { return _selectedStudent; }
             set
             {
-                _test = value;
+                _selectedStudent = value;
                 OnPropertyChanged();
             }
         }
 
-        private void RefreshStudents(object obj)
+        private ObservableCollection<Student> _students;
+
+        public ObservableCollection<Student> Students
         {
-            MessageBox.Show(Test);
+            get { return _students; }
+            set
+            {
+                _students = value;
+                OnPropertyChanged();
+            }
         }
 
+
+        private int _selectedGroupId;
+
+        public int SelectedGroupId
+        {
+            get { return _selectedGroupId; }
+            set
+            {
+                _selectedGroupId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Group> _group;
+
+        public ObservableCollection<Group> Groups
+        {
+            get { return _group; }
+            set
+            {
+                _group = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private void RefreshStudents(object obj)
+        {
+
+        }
+
+        private void InitGroups()
+        {
+            Groups = new ObservableCollection<Group>
+            {
+                new Group { Id = 0, Name = "Wszystkie" },
+                new Group { Id = 1, Name = "1A" },
+                new Group { Id = 2, Name = "2A" },
+            };
+
+            SelectedGroupId = 0;
+        }
 
     }
 }
